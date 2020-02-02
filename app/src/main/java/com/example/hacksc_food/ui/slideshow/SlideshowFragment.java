65a67e0a5712321373e95578eb57d4c57c32f241
Stephen Quiton <quiton@usc.edu>
2020-02-02 00:18:01 -0800

    Merge food list to master

--- app/src/main/java/com/example/hacksc_food/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/hacksc_food/ui/slideshow/SlideshowFragment.java
@@ -11,6 +11,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.hacksc_food.R;
 
@@ -23,13 +25,27 @@ public class SlideshowFragment extends Fragment {
         slideshowViewModel =
                 ViewModelProviders.of(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(this, new Observer<String>() {
+
+        RecyclerView recyclerView = (RecyclerView) root.findViewById(R.id.nav_recycle);
+        ListAdapter listAdapter = new ListAdapter(
+                getActivity(),
+                sample_data.titles,
+                sample_data.tags,
+                sample_data.meal_descriptions,
+                sample_data.num_people,
+                sample_data.meal_times,
+                sample_data.meal_addresses
+        );
+        recyclerView.setAdapter(listAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+       // final TextView textView = root.findViewById(R.id.text_slideshow);
+/*        slideshowViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
         return root;
     }
 }
---
+++ app/src/main/java/com/example/hacksc_food/ui/slideshow/sample_data.java
@@ -0,0 +1,85 @@
+package com.example.hacksc_food.ui.slideshow;
+
+public class sample_data {
+    public static String[] titles = new  String[]{
+            "Pepperoni Pizza",
+            "Ramen Noodles",
+            "Extra Zenify",
+            "C4 Orange Slice flavor",
+            "Bacon-meatball spaghetti",
+            "Fried Rice",
+            "Pad Thai",
+            "Korean BBQ",
+            "Chicken Adobo",
+            "Screwdriver",
+            "Galaxy Note 7"
+    };
+
+    public static String[] tags = new String[]{
+            "#mamamia",
+            "#JustinTimberlake",
+            "#mlh2020",
+            "#nosleep",
+            "#mamamia",
+            "#asian",
+            "#asian",
+            "#asian",
+            "#asian",
+            "#drill-bits",
+            "#ExplosiveFlavor"
+    };
+
+    public static String[] num_people = new String[]{
+            "1",
+            "1",
+            "3",
+            "4",
+            "1",
+            "1",
+            "2",
+            "1",
+            "2",
+            "2",
+            "2"
+    };
+    public static String[] meal_times = new String[]{
+            "14:55",
+            "15:30",
+            "14:55",
+            "12:55",
+            "08:55",
+            "14:00",
+            "14:30",
+            "12:43",
+            "10:00",
+            "06:00",
+            "14:55"
+    };
+
+    public static String[] meal_addresses = new String[]{
+            "some address",
+            "some address",
+            "some address",
+            "some address",
+            "some address",
+            "some address",
+            "some address",
+            "some address",
+            "some address",
+            "some address",
+            "some address"
+    };
+    public static String[] meal_descriptions = new String[]{
+            "some description",
+            "some description",
+            "some description",
+            "some description",
+            "some description",
+            "some description",
+            "some description",
+            "some description",
+            "some description",
+            "some description",
+            "some description",
+    };
+}
