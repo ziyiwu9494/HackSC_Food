e5b70fbf986c537a8349475c03b3ee85c57de571
Stephen Quiton <quiton@usc.edu>
2020-02-01 23:21:37 -0800

    Merge food list to master

--- app/src/main/java/com/example/hacksc_food/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/hacksc_food/ui/home/HomeFragment.java
@@ -4,25 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 //import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.hacksc_food.ListAdapter;
-import com.example.hacksc_food.Meal;
 import com.example.hacksc_food.R;
-import com.example.hacksc_food.sample_data;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class HomeFragment extends Fragment {
 
@@ -39,7 +29,15 @@ public class HomeFragment extends Fragment {
         RecyclerView recyclerView = (RecyclerView) root.findViewById(R.id.nav_recycle);
 
 
-        ListAdapter listAdapter = new ListAdapter(getActivity(), sample_data.titles, sample_data.tags);
+        ListAdapter listAdapter = new ListAdapter(
+                getActivity(),
+                sample_data.titles,
+                sample_data.tags,
+                sample_data.meal_descriptions,
+                sample_data.num_people,
+                sample_data.meal_times,
+                sample_data.meal_addresses
+        );
         recyclerView.setAdapter(listAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
--- app/src/main/java/com/example/hacksc_food/ui/home/ListAdapter.java
+++ app/src/main/java/com/example/hacksc_food/ui/home/ListAdapter.java
@@ -1,25 +1,31 @@
-package com.example.hacksc_food;
+package com.example.hacksc_food.ui.home;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
-import org.w3c.dom.Text;
+import com.example.hacksc_food.R;
 
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.MyViewHolder> {
 
-    String data1[], data2[];
+    String title_data[], tag_data[], desc_data[], num_people_data[],time_data[],address_data[];
     Context context;
 
-    public ListAdapter(Context ct, String s1[], String s2[]){
+    public ListAdapter(Context ct, String s1[], String s2[], String s3[], String s4[], String s5[], String s6[]){
         context = ct;
-        data1 = s1;
-        data2 = s2;
+        title_data = s1;
+        tag_data = s2;
+        desc_data = s3;
+        num_people_data= s4;
+        time_data= s5;
+        address_data = s6;
     }
 
     @NonNull
@@ -31,23 +37,38 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.MyViewHolder> 
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        holder.myText1.setText(data1[position]);
-        holder.myText2.setText(data2[position]);
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
+        holder.myText1.setText(title_data[position]);
+        holder.myText2.setText(tag_data[position]);
+        holder.mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context,find_meal_details.class);
+                intent.putExtra("title_data", title_data[position]);
+                intent.putExtra("tag_data", tag_data[position]);
+                intent.putExtra("desc_data", desc_data[position]);
+                intent.putExtra("num_people_data", num_people_data[position]);
+                intent.putExtra("time_data", time_data[position]);
+                intent.putExtra("address_data", address_data[position]);
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return data1.length;
+        return title_data.length;
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
         TextView myText1, myText2;
 
+        ConstraintLayout mainLayout;
         public MyViewHolder(@NonNull View itemView){
             super(itemView);
             myText1 = itemView.findViewById(R.id.titleText);
-            myText2 = itemView.findViewById(R.id.descText);
+            myText2 = itemView.findViewById(R.id.tagText);
+            mainLayout = itemView.findViewById(R.id.my_row_layout);
         }
     }
 
